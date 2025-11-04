75a54b4f37ca5f6bb5036afe51a3d1f947fa1a2f
Aung Mrat Kyaw <aungmratkyaw1985@gmail.com>
2025-11-04 11:48:28 +0630

    This is batch3 team project

---
+++ FacilityManagement/src/main/java/com/mycompany/facilitymanagement/FacilityManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.facilitymanagement;
+
+/**
+ *
+ * @author aungmratkyaw
+ */
+public class FacilityManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
